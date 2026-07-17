3e11952a487bf6a40ff070315ec0ac14f3719399
showemeowkx <sashacherepov55@gmail.com>
2026-07-17 19:54:44 +0200

    Feat: add Docker support for backend and frontend services

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
 })
 export class AppModule {}
